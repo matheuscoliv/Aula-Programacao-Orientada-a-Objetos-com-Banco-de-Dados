4337b96e10d653941a6e9a88e66fbadd2367358a
Matheus Henrique Costa Oliveira <matheushenrll@outlook.com>
2026-08-26 17:15:52 -0300

    Resoluçao do exercicio da aula 2

--- src/main/java/org/example/controller/Produtos.java
+++ src/main/java/org/example/controller/Produtos.java
@@ -1,4 +1,27 @@
 package org.example.controller;
 
-public class Produtos {
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Produtos implements Initializable {
+
+    @FXML
+    private TextField campoNome, campoPreco, campoQuantidadeEstoque;
+    @FXML
+    public void aoSalvar() {
+        String nome = campoNome.getText();
+        String preco = campoPreco.getText();
+        String quantidade = campoQuantidadeEstoque.getText();
+
+        System.out.printf("\nProdutos: %s\nPreco: %s\nEstoque: %s", nome, preco, quantidade);
+    }
+    @Override
+    public void initialize(URL local, ResourceBundle rb) {
+        campoNome.setPromptText("Digite o nome do produto");
+    }
 }
